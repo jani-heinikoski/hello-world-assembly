f6d72e798338b146eb172551e98aa8b460ddc1c2
Jani Heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2023-02-19 18:36:25 +0200

    updated labels

--- hello_world.s
+++ hello_world.s
@@ -9,15 +9,18 @@
 message:
     # Beginning of the string literal "Hello world\n\0" is at the memory location of message
     .ascii "Hello world\n\0"
-syscall_write_opcode:
-    # 32-bit representation of the integer 1 is at the memory location of syscall_write_opcode
+syscall_write_number:
+    # 32-bit representation of the integer 1 is at the memory location of syscall_write_number
     .long 1
+syscall_exit_number:
+    # 32-bit representation of the integer 60 is at the memory location of syscall_exit_number
+    .long 60
 
 # Text section where primarily the unaltered instructions are.
 .text
 _start:
-    # Move value at location syscall_write_opcode into reg. eax (note: uses rip-relative addressing for PIE support)
-    movl    syscall_write_opcode(%rip), %eax
+    # Move value at location syscall_write_number into reg. eax (note: uses rip-relative addressing for PIE support)
+    movl    syscall_write_number(%rip), %eax
     # Move immediate value 1 into reg. edi
     movl    $1, %edi
     # Loads the effective address of message label into register rsi (note: uses rip-relative addressing for PIE support)
@@ -27,8 +30,8 @@ _start:
     # Make the system call
     syscall
 
-    # Move immediate value 60 into reg. eax
-    movl    $60, %eax
+    # Move value at location syscall_exit_number into reg. eax (note: uses rip-relative addressing for PIE support)
+    movl    syscall_exit_number(%rip), %eax
     # Move immediate value 0 into reg. edi
     movl    $0, %edi
     # Make the system call
